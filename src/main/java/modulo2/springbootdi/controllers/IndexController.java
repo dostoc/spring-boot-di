f491fae3a9c33c02b6c22cfe8149ac4d8d44d0e9
dostoc <jona.ky@gmail.com>
2023-10-04 16:25:18 -0300

    inicio de proyecto

---
+++ src/main/java/modulo2/springbootdi/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package modulo2.springbootdi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import modulo2.springbootdi.models.service.IServicio;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private IServicio servicio;
+
+    @GetMapping({"/","","/index"})
+    public String index(Model model){
+        model.addAttribute("objeto", servicio.operacion());
+        return "index";
+    }
+}
---
+++ src/main/java/modulo2/springbootdi/models/service/IServicio.java
@@ -0,0 +1,8 @@
+package modulo2.springbootdi.models.service;
+
+public interface IServicio {
+
+    public String operacion();
+    
+
+}
